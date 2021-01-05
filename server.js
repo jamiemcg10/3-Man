db951dd00b4d49386e47ffe949fd2b40517c44fd
jamiemcg10 <jamie.smart.10@gmail.com>
2021-01-05 11:51:09 -0500

    server redirect to https

--- server.js
+++ server.js
@@ -40,7 +40,14 @@ app.use(bodyParser.json());
 // Routing
 app.get('/', function(req, res) {
     console.log(req);
-    res.sendFile(path.join(__dirname, 'index.html'));
+    console.log(req.secure);
+    if (!req.secure && req.headers.host === "www.3mangame.com"){
+        res.redirect(302, "https://www.3mangame.com/");
+    } else {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    };
+
+    
 });
 
 app.get('/contact', function(req, res) {
